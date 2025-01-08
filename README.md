538ae2d14db9da791c3f0b8d02e349e9d73007aa
manilomonilo <suitlemon5@gmail.com>
2025-01-08 19:58:50 +0100

    finish the app

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Hi, This is a calculator app for applying my js skills ability, If you have any recommendations or feedback please share it with me ;).
 
-![dark theme app view](imgs/the-app-dark-theme.png) ![light theme app view](imgs/the-app-light-theme.png)
 
 [live page link](https://manilomonilo.github.io/calculator/)
+
+Dark theme             |  Light theme
+:-------------------------:|:-------------------------:
+![dark theme app view](imgs/the-app-dark-theme.png)  |   ![light theme app view](imgs/the-app-light-theme.png)
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let switchMode = document.querySelector(".icon-button");
-let calculator = document.querySelector(".calculator");
-let icon = document.querySelector(".icon");
-let equal = document.querySelector(".equals");
-let calculatorNumbers = document.querySelector(".calculator-buttons");
-let buttons = document.querySelectorAll(".button");
-let operators = document.querySelectorAll(".operator")
-let iconButton = document.querySelector(".button")
+const switchMode = document.querySelector(".icon-button");
+const calculator = document.querySelector(".calculator");
+const icon = document.querySelector(".icon");
+const equal = document.querySelector(".equals");
+const calculatorNumbers = document.querySelector(".calculator-buttons");
+const buttons = document.querySelectorAll(".button");
+const operators = document.querySelectorAll(".operator")
+const iconButton = document.querySelector(".button")
 
 // switch color theme
 
@@ -33,7 +33,6 @@ if (localStorage.getItem("theme") === "dark") {
 switchMode.addEventListener("click", () => {
     if (localStorage.getItem("theme") === null) {
         localStorage.setItem("theme", "light");
-        console.log("yoooo")
     }
     if (localStorage.getItem("theme") === "light") {
         switchToDarkMood();
@@ -57,22 +56,126 @@ switchMode.addEventListener("click", () => {
     }
 });
 
-// calculating
+// display the content
 
-let displayed = document.querySelector(".calculator-display");
+const displayed = document.querySelector(".calculator-display");
 
 function displayElement(element) {
     displayed.textContent += element.textContent;
 }
 
+// an object has the operations availble on the calculator and give each one it's precedence
+const operationsPrecedence = {
+    "+": 1,
+    "-": 1,
+    "×": 2,
+    "÷": 2,
+    "%": 2
+};
+
+// turn infix notation expression into postfix notation expression
+function calculate(operation) {
+    // turn the operation into an array
+    operation = operation.split(/([%÷×−+])/);
+
+    // turn infix into postfix
+    let queue = []; // FIFO 123, 123 push(in), shift(out)
+    let operationStack = []; // LIFO 123, 321 push(in), pop(out)
+
+    // loop through the operation
+    for (let char of operation) {
+        // if the current character is a digit push it into the queue
+        if (/\d+/.test(char)) {
+            queue.push(char);
+        }
+        // if the current character is an operator
+        else if (/[%÷×−+]/.test(char)) {
+            // if the stack is empty just push the character
+            if (operationStack.length === 0) {
+                operationStack.push(char);
+            }
+            // otherwise the stack is not empty 
+            else {
+                // compair between the precedence of the current character "which is necesserly an operator" and the one at the top of the stack
+
+                // if the current character has a greater precedence
+                if (operationsPrecedence[char] > operationsPrecedence[operationStack[operationStack.length - 1]]) {
+                    // push it into the stack
+                    operationStack.push(char);
+
+                }
+                // else it's equal in precedence or the top operator on the stack is greater precedence
+                else {
+                    // loop through the stack and push the operator from it to the queue
+                    for (let ope of operationStack) {
+                        // push the operator into queue
+                        queue.push(ope);
+                        // remove the operator from the stack
+                        operationStack.pop();
+                    }
+                    operationStack.push(char);
+                }
+            }
+        }
+    }
+    for (let ope of operationStack) {
+        queue.push(ope);
+        operationStack.pop()
+    }
+
+    // change −, ÷, ×
+    queue = queue.toString().replaceAll(",", " "); // turn queue to a string 
+    queue = queue
+        .replace(/−/g, '-')   // Replace − with -
+        .replace(/÷/g, '/')   // Replace ÷ with /
+        .replace(/×/g, '*');  // Replace × with *
+
+    queue = queue.split(" "); // get it back to be an array
+
+    // calculat it and return the result
+
+    let resultStack = [];
+    let evaluate;
+
+    for (let element of queue) {
+        // if we have element is a number then add it to resultStack
+        if (/\d+/.test(element)) {
+            resultStack.push(element);
+        } 
+        // otherwise evaluate the two numbers currently in resultStack and add the result to the resultStack again
+        else {
+            evaluate = resultStack[0];
+            evaluate += element;
+            evaluate += resultStack[1];
+            // overide the reusltStack with a new array has a result of the evaluation
+            resultStack = [eval(evaluate)];
+        }
+    }
+
+    return resultStack[0];
+}
+
+const resultDisplay = document.querySelector(".display-result");
+
 buttons.forEach(element => {
     element.addEventListener("click", () => {
         if (element.classList.contains("icon-button")) {
-            console.log("icon is here");
-        } else if(element.classList.contains("delete")){
             return;
-        } else if(element.classList.contains("equals")) {
-            console.log("equals is here")
+        } else if ((displayed.textContent === ("0") || /^.+[%÷×−+.]$/.test(displayed.textContent)) && /^[%÷×−+.]$/.test(element.textContent)) {
+            return;
+        } else if (element.classList.contains("delete")) {
+            displayed.textContent = displayed.textContent.slice(0, -1);
+            if (displayed.textContent === "") {
+                displayed.textContent = "0";
+            }
+        } else if (element.classList.contains("equals")) {
+            if (/^.+[%÷×−+.]$/.test(displayed.textContent)) {
+                return
+            } else {
+                let result = calculate(displayed.textContent);
+                resultDisplay.textContent = result;
+                displayed.textContent = "0";
+            }
         } else if (displayed.textContent === "0") {
             displayed.textContent = "";
             displayElement(element);
@@ -81,5 +184,3 @@ buttons.forEach(element => {
         }
     });
 });
-
-
